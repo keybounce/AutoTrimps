1a0f44840e08a3995235f90a7112dd21b4fab685
keybounce <michael-github@stb04.nccom.com>
2018-11-19 21:01:03 -0800

    Test branch. This is a .user.js / AutoTrimps2.js that serves "testBranch" from raw.githubusercontent.com
    Note that this may be changed at any time -- "testBranch" is a label subject to hard resets for any testing.

--- .user.js
+++ .user.js
@@ -1,22 +1,24 @@
 // ==UserScript==
-// @name         AutoTrimps-Zek
-// @version      1.0-Zek
-// @namespace    https://Zorn192.github.io/AutoTrimps
-// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
-// @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
+// @name         AutoTrimps-Keybounce-testBranch
+// @version      1.2-Keybounce
+// @namespace    https://raw.githubusercontent.com/keybounce/AutoTrimps/testBranch
+// @updateURL    https://raw.githubusercontent.com/keybounce/AutoTrimps/testBranch/.user.js
+// @description  Keybounce's test branch, may change at any time
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, keybounce
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *Zorn192.github.io/AutoTrimps*
+// @connect      *Keybounce.github.io/AutoTrimps*
+// @connect      *githubusercontent.com/keybounce*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
+script.id = 'AutoTrimps-Keybounce-testing';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+// script.src = 'https://Keybounce.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://raw.githubusercontent.com/keybounce/AutoTrimps/testBranch/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
