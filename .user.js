f9ab9b19becd4946a57ad807ae48abf83373cfb6
keybounce <michael-github@stb04.nccom.com>
2018-11-20 23:11:33 -0800

    Didn't quite understand how those URL's worked. This should make a working test branch loader.

--- .user.js
+++ .user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoTrimps-Keybounce-testBranch
 // @version      1.2-Keybounce
-// @namespace    https://raw.githubusercontent.com/keybounce/AutoTrimps/testBranch
-// @updateURL    https://raw.githubusercontent.com/keybounce/AutoTrimps/testBranch/.user.js
+// @namespace    https://raw.githubusercontent.com/keybounce/AutoTrimps/TestRoot
+// @updateURL    https://raw.githubusercontent.com/keybounce/AutoTrimps/TestRoot/.user.js
 // @description  Keybounce's test branch, may change at any time
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, keybounce
 // @include      *trimps.github.io*
@@ -18,7 +18,7 @@ var script = document.createElement('script');
 script.id = 'AutoTrimps-Keybounce-testing';
 //This can be edited to point to your own Github Repository URL.
 // script.src = 'https://Keybounce.github.io/AutoTrimps/AutoTrimps2.js';
-script.src = 'https://raw.githubusercontent.com/keybounce/AutoTrimps/testBranch/AutoTrimps2.js';
+script.src = 'https://raw.githubusercontent.com/keybounce/AutoTrimps/TestRoot/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
